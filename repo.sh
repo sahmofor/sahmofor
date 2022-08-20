f03408b26c86ac685123ede5e67266ed37a009f8
Cloud User <ec2-user@ip-172-31-89-165.ec2.internal>
2022-08-20 19:23:21 +0000

    script to create a repository created

---
+++ repo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Script to create a new repository
+echo "# Ignatius" >> README.md
+git init
+git add README.md
+git commit -m "second commit"
+git branch -M Dev
+git remote add origin https://github.com/sahmofor/Team-16-project.git
+git push -u origin Dev
