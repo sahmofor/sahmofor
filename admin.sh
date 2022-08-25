6da12a701f7ae9b12eac7a9ba5d3ac06188dbb15
Cloud User <ec2-user@ip-172-31-89-165.ec2.internal>
2022-08-25 03:00:30 +0000

    backup completed

--- admin.sh
+++ admin.sh
@@ -4,6 +4,7 @@ echo "please enter your name"
 read name
 echo "Please enter the fee amount that you can pay for the course"
 read fee
+echo "I like school"
 if [ $fee -ge 3500 ]
 then
 echo "Congratulations $name, your are admitted"
